0db151112390ce9388e14115bf1ec99cbbd67083
Jeffery Cosby <jcoz1968@gmail.com>
2019-10-04 23:12:08 -0500

    versioning and load balancing

--- server/service.js
+++ server/service.js
@@ -30,8 +30,13 @@ module.exports = (config) => {
     return res.json({ result: serviceKey });
   });
 
-  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
-    return next('not implemented exception');
+  service.get('/find/:servicename/:serviceversion', (req, res) => {
+    const { servicename, serviceversion } = req.params;
+    const svc = serviceRegistry.get(servicename, serviceversion);
+    if (!svc) {
+      return res.status(404).json({ result: 'Service not found.' });
+    }
+    return res.json(svc);
   });
 
   // eslint-disable-next-line no-unused-vars
